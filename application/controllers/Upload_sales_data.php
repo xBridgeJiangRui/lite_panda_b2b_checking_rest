063a34a48cb1ef4722813553cd467a1eeacadfc5
jiangruigoh <jiangrui.goh@pandasoftware.my>
2022-08-20 10:11:12 +0800

    jiangrui
    commit update for everrise hq

--- application/controllers/Upload_sales_data.php
+++ application/controllers/Upload_sales_data.php
@@ -360,5 +360,159 @@ class Upload_sales_data extends REST_Controller{
         );
         echo json_encode($response);die;
     }
+
+    public function everrise_sku_cs_date_get()
+    {
+        ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', 0);        
+        $database = 'b2b_hub';
+        $table = 'acc_trans_code_sku_cs_date';
+        $query = "SELECT * FROM $database.$table WHERE exported = 0 ORDER BY supcus_code ASC LIMIT 100";
+        $data = $this->db->query("$query");
+        $customer_guid_array = $this->db->query("SELECT customer_guid FROM rest_api.`run_once_config` WHERE active = 1 LIMIT 1");
+        $customer_guid = $customer_guid_array->row('customer_guid');
+        $message = '';
+        $i = 0;
+        $ii = 0;
+        $iii = 0;
+        $time_start = microtime(true);
+
+        if($customer_guid_array->num_rows() <= 0)
+        {
+            $this->response(
+                [
+                    'status' => TRUE,
+                    'message' => 'Rest api config not active'
+                ]
+            );             
+        }
+
+        if($data->num_rows() <= 0)
+        {
+            $this->response(
+                [
+                    'status' => TRUE,
+                    'message' => 'No record to sync'
+                ]
+            );             
+        }
+        // print_r($data->num_rows());die;
+
+        foreach($data->result() as $row)
+        {
+            $iii++;
+             //echo $row->RefNo;die; 
+            $acc_refno = $row->refno;
+            $operation = $row->operation;
+            $database2 = 'report_summary';
+            $table2 = 'sku_cs_date';
+            // jr edited due to sku cs date cant get data and everrise want to divide to two quater upload sales - 26/04/2022
+            $get_acc_trans = $this->db->query("SELECT refno,date_from,date_to,supcus_code FROM backend.acc_trans WHERE refno = '$acc_refno'");
+            $get_acc_trans_c2 = $this->db->query("SELECT trans_guid,outlet FROM backend.acc_trans_c2 a WHERE a.`trans_guid` = '$acc_refno' LIMIT 1");
+            $sup_code = $get_acc_trans->row('supcus_code');
+            $start_date = $get_acc_trans->row('date_from'); 
+            $end_date = $get_acc_trans->row('date_to'); 
+            $loc_group = $get_acc_trans_c2->row('outlet'); 
+
+            if($sup_code == '' || $sup_code == 'null' || $sup_code == null || $start_date == '' || $start_date == 'null' || $start_date == null 
+            || $end_date == '' || $end_date == 'null' || $end_date == null || $loc_group == '' || $loc_group == 'null' || $loc_group == null)
+            {
+            $run = $this->db->query("UPDATE $database.$table SET exported = 9,exported_at = NOW() WHERE refno = '$acc_refno'"); 
+                $ii++;
+                continue;
+            }
+            //$query2 = "SELECT * FROM $database2.$table2 WHERE acc_refno = '$acc_refno'";
+            $query2 = "SELECT * FROM report_summary.sku_cs_date WHERE sup_code = '$sup_code' AND location = '$loc_group' AND bizdate BETWEEN '$start_date' AND '$end_date'";
+            // echo $query2;die;
+            $result = $this->db->query("$query2");
+            // echo $this->db->last_query();die;
+            if($result->num_rows() > 0)
+            {   
+                $database3 = 'backend';
+                $table3 = 'acc_trans';
+                $acc_total = $this->db->query("SELECT ROUND(SUM(amount),2) as acc_total FROM $database3.$table3 WHERE refno = '$acc_refno'");
+                // echo $this->db->last_query();die;
+                // echo $acc_total->row('acc_total');
+                // die;
+
+                //$sum_query = "SELECT ROUND(SUM(Cost_CS),2) as cost FROM $database2.$table2 WHERE acc_refno = '$acc_refno'";
+                $sum_query = "SELECT ROUND(SUM(Cost_CS),2) AS cost FROM report_summary.sku_cs_date WHERE sup_code = '$sup_code' AND location = '$loc_group' AND bizdate BETWEEN '$start_date' AND '$end_date'";
+                // end jr edited 26/04/2022
+                $result_sum = $this->db->query("$sum_query");
+
+                // echo $result_sum->row('cost');
+                $variance = $result_sum->row('cost') -$acc_total->row('acc_total');
+                // echo $variance; 
+                if($variance <= 1.00)
+                {
+                    // echo 'valid';
+                }
+                else
+                {
+                    $run = $this->db->query("UPDATE $database.$table SET exported = 9,exported_at = NOW() WHERE refno = '$acc_refno'"); 
+                    $ii++;
+                    continue;
+                }                  
+                // echo 1;die;
+                $data = array(
+                    'customer_guid' => $customer_guid,
+                    'operation' => $operation,
+                    'refno' => $acc_refno,
+                    'result' => $result->result(),
+                );
+                // print_r($data);die;
+                $username = 'admin'; //get from rest.php
+                $password = '1234'; //get from rest.php
+
+                //$url = 'http://127.0.0.1/PANDA_GITHUB/rest_b2b/index.php/Get_sales/sku_cs_date';
+                // $url = 'http://127.0.0.1/rest_b2b/index.php/Get_sales/sku_cs_date';
+                $url = $this->b2b_ip.'/rest_b2b/index.php/Get_sales/sku_cs_date_everrise';
+		        // edited by jr sku_cs_date - 26/04/2022
+                $ch = curl_init($url);
+                // echo $url;die;
+
+                curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Api-KEY: 123456"));
+                curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+                $result = curl_exec($ch);
+                // echo $result;die;
+                $output =  json_decode($result);
+                 //print_r($output);die;
+                $status = $output->status;
+                //echo $status;die;
+                if($status == "true")
+                {
+                    $run = $this->db->query("UPDATE $database.$table SET exported = 1,exported_at = NOW() WHERE refno = '$acc_refno'"); 
+                    $i++;
+                    // echo 1;die;
+                }
+                else
+                {
+                    // echo $iii.'error';die;
+                    // $run = $this->db->query("UPDATE $database.$table SET exported = 99,exported_at = NOW() WHERE refno = '$acc_refno'"); 
+                }
+            }
+            else
+            {
+                $run = $this->db->query("UPDATE $database.$table SET exported = 9,exported_at = NOW() WHERE refno = '$acc_refno'"); 
+                $ii++;
+            }
+        }//close foreach
+        $time_end = microtime(true);
+        $execution_time = ($time_end - $time_start);
+        $message = 'Used time '.$execution_time.' Uploaded '.$i.' record'.'    '.$ii.' sales data record not found';
+        $this->response(
+            [
+                'status' => TRUE,
+                'message' => $message
+            ]
+            // $this->Main_model->query_call('Api','login_validation_get', $data)
+        ); 
+    }
 }
 
